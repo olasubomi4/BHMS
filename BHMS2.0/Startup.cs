773b43473ae7030724abdccb4d67e435cfce3776
olasubomi odekunle <olasubomiodekunle@gmail.com>
2021-11-16 17:39:19 +0100

    new

---
+++ BHMS2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BHMS2._0.Startup))]
+namespace BHMS2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
