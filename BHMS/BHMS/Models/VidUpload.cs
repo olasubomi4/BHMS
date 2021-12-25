ba824981265abad3345196b447f2d71744a27bcd
olasubomi4 <40355148+olasubomi4@users.noreply.github.com>
2021-12-25 04:35:52 +0100

    COMPARING TO ITEM BRANCH

---
+++ BHMS/BHMS/Models/VidUpload.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BHMS.Models
+{
+    public class VidUpload
+    {
+        public string UploadResult { get; set; }
+    }
+}
